92521341435d46ea8a8e31e07255f7d424bd1c8a
thomasdavis <thomasalwyndavis@gmail.com>
2014-08-22 00:41:58 +1000

    butchering the design

--- _assets/javascripts/map.js
+++ _assets/javascripts/map.js
@@ -20,8 +20,10 @@ function initMap(el)
 {
 	map = L.map($(MAP_ELEMENT)[0]).setView(DEFAULT_COORDS, DEFAULT_ZOOM);
 
-	var mapBG = L.tileLayer('http://{s}.tile.stamen.com/toner-background/{z}/{x}/{y}.png', {
-		attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+	//var mapBG = L.tileLayer('http://{s}.tile.stamen.com/toner-background/{z}/{x}/{y}.png', {
+	var mapBG = L.tileLayer('', {
+		//attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+		attribution: '',
 		subdomains: 'abcd',
 		minZoom: 0,
 		maxZoom: 20
@@ -43,9 +45,9 @@ function showElectorates(geojson)
 		style : function(feature) {
 			// :TODO: finalise pallete and hookup to legislator data
 			var colors = [
-				'#D3CEAA',
-				'#8E001C',
-				'#1D8E00'
+				'#fff',
+				'#fff',
+				'#fff'
 			];
 			var picked = colors[Math.floor(Math.random() * colors.length)];
 
