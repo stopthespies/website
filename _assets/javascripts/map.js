584e33887688656a40dd7130a2443feeb6a1ca56
pospi <pospi@spadgos.com>
2014-08-25 21:11:19 +1000

    disable map scrollwheel, its annoying

--- _assets/javascripts/map.js
+++ _assets/javascripts/map.js
@@ -18,7 +18,9 @@ var featureSelect;
 
 function initMap(el)
 {
-	map = L.map($(MAP_ELEMENT)[0]).setView(DEFAULT_COORDS, DEFAULT_ZOOM);
+	map = L.map($(MAP_ELEMENT)[0], {
+		scrollWheelZoom: false
+	}).setView(DEFAULT_COORDS, DEFAULT_ZOOM);
 
 	//var mapBG = L.tileLayer('http://{s}.tile.stamen.com/toner-background/{z}/{x}/{y}.png', {
 	var mapBG = L.tileLayer('', {
