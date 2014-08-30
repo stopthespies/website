29380a62026caf95a75680530f11ccf3cc5ad399
pospi <pospi@spadgos.com>
2014-08-30 13:58:51 +1000

    dont need base map layer, we have shapes

--- _assets/javascripts/map.js
+++ _assets/javascripts/map.js
@@ -22,15 +22,6 @@ function initMap(el)
 		scrollWheelZoom: false
 	}).setView(DEFAULT_COORDS, DEFAULT_ZOOM);
 
-	//var mapBG = L.tileLayer('http://{s}.tile.stamen.com/toner-background/{z}/{x}/{y}.png', {
-	var mapBG = L.tileLayer('', {
-		//attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-		attribution: '',
-		subdomains: 'abcd',
-		minZoom: 0,
-		maxZoom: 20
-	}).addTo(map);
-
 	$.ajax({
 		url: '/map/electorates.json',
 		dataType: "json",
