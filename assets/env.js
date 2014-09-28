db5e4a5886d5517be1499d034f331daa5739a0a4
pospi <pospi@spadgos.com>
2014-09-28 13:49:22 +1000

    Site updated at 2014-09-28 03:49:22 UTC

--- assets/env.js
+++ assets/env.js
@@ -8,15 +8,15 @@ var STS = {
 
     LEGISLATORS_LOCATOR_URL : 'https://test.stopthespies.org/legislators',
 
-    API_BASE_URL            : 'https://test.stopthespies.org:443/api',
+    API_BASE_URL            : 'https://api.stopthespies.org:443',
     API_SOCKET_BASEURL      : '/api',
 
-    TWEETS_READ_URL         : 'https://test.stopthespies.org:443/api/tweets',
-    STATS_READ_URL          : 'https://test.stopthespies.org:443/api/stats',
-    SHARES_READ_URL          : 'https://test.stopthespies.org:443/api/shares',
+    TWEETS_READ_URL         : 'https://api.stopthespies.org:443/tweets',
+    STATS_READ_URL          : 'https://api.stopthespies.org:443/stats',
+    SHARES_READ_URL          : 'https://api.stopthespies.org:443/shares',
 
-    SEND_EMAIL_URL          : 'https://test.stopthespies.org:443/api/email',
-    LOG_URL_BASE            : 'https://test.stopthespies.org:443/api/log',
+    SEND_EMAIL_URL          : 'https://api.stopthespies.org:443/email',
+    LOG_URL_BASE            : 'https://api.stopthespies.org:443/log',
 
     USER_PROGRESS_COOKIE_LIFETIME : 28 * 24 * 3600 // ~1 month
   },
