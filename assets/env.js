82ec33e2701a37714b8ec697695dc94c9c8f6b8b
Thomas Davis <thomasalwyndavis@gmail.com>
2014-09-29 23:38:34 +1000

    Site updated at 2014-09-29 13:38:34 UTC

--- assets/env.js
+++ assets/env.js
@@ -4,20 +4,20 @@ var STS = {
     ABSURL                  : 'https://test.stopthespies.org',
     BASEURL                 : '',
 
-    ENABLE_REALTIME         : 0,
+    ENABLE_REALTIME         : 1,
     SOCKET_CONNECT_TIMEOUT  : 1000,
 
     LEGISLATORS_LOCATOR_URL : 'https://test.stopthespies.org/legislators',
 
-    API_BASE_URL            : 'https://api.stopthespies.org:443',
-    API_SOCKET_BASEURL      : '/api',
+    API_BASE_URL            : 'https://test.stopthespies.org:443/api',
+    API_SOCKET_BASEURL      : 'api/socket.io',
 
-    TWEETS_READ_URL         : 'https://api.stopthespies.org:443/tweets',
-    STATS_READ_URL          : 'https://api.stopthespies.org:443/stats',
-    SHARES_READ_URL          : 'https://api.stopthespies.org:443/shares',
+    TWEETS_READ_URL         : 'https://test.stopthespies.org:443/api/tweets',
+    STATS_READ_URL          : 'https://test.stopthespies.org:443/api/stats',
+    SHARES_READ_URL          : 'https://test.stopthespies.org:443/api/shares',
 
-    SEND_EMAIL_URL          : 'https://api.stopthespies.org:443/email',
-    LOG_URL_BASE            : 'https://api.stopthespies.org:443/log',
+    SEND_EMAIL_URL          : 'https://test.stopthespies.org:443/api/email',
+    LOG_URL_BASE            : 'https://test.stopthespies.org:443/api/log',
 
     USER_PROGRESS_COOKIE_LIFETIME : 28 * 24 * 3600 // ~1 month
   },
